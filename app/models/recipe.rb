e1aad7d44ab050152f828cfee87d93fe2e12a368
David Good <davidagood@gmail.com>
2016-06-10 22:46:35 +0100

    fixing non-env var hostport

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,7 +4,7 @@ class Recipe
   include HTTParty
 
   @key_value = ENV["FOOD2FORK_KEY"]
-  @hostport = ENV["FOOD2FORK_SERVER_AND_PORT"] || 'http://food2fork.com'
+  @hostport = ENV["FOOD2FORK_SERVER_AND_PORT"] || 'food2fork.com'
   base_uri "http://#{@hostport}/api"
   #base_uri 'http://food2fork.com/api'
   default_params key: @key_value#ENV["FOOD2FORK_KEY"]
